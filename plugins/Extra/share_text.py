97305294d944b6dbc92476b3eaaa1044ab9e5d1a
MS DHONI <137499419+sbplays10@users.noreply.github.com>
2024-06-08 20:09:28 +0530

    Update share_text.py

--- plugins/Extra/share_text.py
+++ plugins/Extra/share_text.py
@@ -15,8 +15,8 @@ async def share_text(client, message):
         input_text = vj.text or vj.caption
     else:
         await vj.reply_text(
-            text=f"**Notice:**\n\n1. Send Any Text Messages.\n2. No Media Support\n\n**Any Question Join Support Chat**",               
-            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Updates Channel", url=f"https://t.me/vj_botz")]])
+            text=f"**Notice:**\n\n1. Send Any Text Messages.\n2. No Media Support\n\n**Any Question Talk To Bot Owner**",               
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Owner", url=f"https://t.me/GroupLeaderr")]])
             )                                                   
         return
     await vj.reply_text(
