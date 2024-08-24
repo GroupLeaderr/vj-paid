e86dbbd7e9cf77ed5aeef79e65d64593c94b3483
GroupLeaderr <155065318+GroupLeaderr@users.noreply.github.com>
2024-08-24 09:46:54 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2592,13 +2592,13 @@ async def cb_handler(client: Client, query: CallbackQuery):
                 query.message.id, 
                 InputMediaPhoto(random.choice(PICS))
             )
-        await query.message.edit_text(
+            await query.message.edit_text(
             text="▣ ▢ ▢"
         )
-        await query.message.edit_text(
+            await query.message.edit_text(
             text="▣ ▣ ▢"
         )
-        await query.message.edit_text(
+            await query.message.edit_text(
             text="▣ ▣ ▣"
         )
             reply_markup = InlineKeyboardMarkup(btn)
